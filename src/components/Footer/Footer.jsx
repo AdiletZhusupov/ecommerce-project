914272d0a4ee96dfa00db581f02370ef06bbb5c6
AdiletZhusupov <adilet.zhusupov@gmail.com>
2022-11-03 13:24:42 +0300

    homepage functionality completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import './footer.css'
+
+import {Container, Row, Col, ListGroup, ListGroupItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/images/eco-logo.png'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <Container>
+        <Row>
+          <Col lg='4'>
+            <div className='logo'>
+                <img src={logo} alt='logo'/>
+                <div>
+                  <h1>Multimart</h1>
+                </div>
+            </div>
+            <p className='footer__text mt-4'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam corrupti sequi accusamus, non rem sunt provident. Nisi nihil totam expedita!
+            </p>
+          </Col>
+          <Col lg='3'>
+            <div className='footer__quick-links'>
+              <h4 className='quick__links-title'>Top Categories</h4>
+              <ListGroup>
+
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg='2'>
+
+          </Col>
+          <Col lg='3'>
+
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,35 @@ import heroImg from '../assets/images/hero-img.png'
 import Services from '../services/Services';
 import ProductsList from '../components/UI/ProductsList';
 import products from '../assets/data/products'
+import counterImg from '../assets/images/counter-timer-img.png'
+import Clock from '../components/UI/Clock'
 
 function Home() {
+  const [trendingProducts, setTrendingProducts] = useState([]);
+  const [bestSalesProducts, setBestSalesProducts] = useState([]);
+  const [mobileProducts, setMobileProducts] = useState([]);
+  const [wirelessProducts, setWirelessProducts] = useState([]);
+  const [popularProducts, setPopularProducts] = useState([]);
 
   const year = new Date().getFullYear()
 
+  useEffect(()=>{
+    const filteredTrendingProducts = products.filter(item => item.category === "chair")
+    
+    const filteredBestSalesProducts = products.filter(item => item.category === "sofa")
+    
+    const filteredMobileProducts = products.filter(item => item.category === "mobile")
+    
+    const filteredWirelessProducts = products.filter(item => item.category === "wireless")
+    
+    const filteredPopularProducts = products.filter(item => item.category === "watch")
+
+    setTrendingProducts(filteredTrendingProducts)
+    setBestSalesProducts(filteredBestSalesProducts)
+    setMobileProducts(filteredMobileProducts)
+    setWirelessProducts(filteredWirelessProducts)
+    setPopularProducts(filteredPopularProducts)
+  },[])
 
   return (
     <Helmet title={'Home'}>
@@ -48,7 +72,64 @@ function Home() {
             <Col lg="12" className='text-center'>
               <h2 className='section__title'>Trending Products</h2>
             </Col>
-            <ProductsList/>
+            <ProductsList data={trendingProducts}/>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='best__sales'>
+        <Container>
+          <Row>
+            <Col lg="12" className='text-center'>
+              <h2 className='section__title'>Best Sales</h2>
+            </Col>
+
+            <ProductsList data={bestSalesProducts}/>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='timer__count'>
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+
+              <div className='clock__top-content'>
+                <h4 className='text-white fs-6 mb-2'>Limited Offers</h4>
+                <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
+              </div>
+              <Clock />
+
+              <motion.button whileTap={{scale: 1.2}} className='buy__btn store__btn'>
+                <Link to='/shop'>Visit Store</Link>
+              </motion.button>
+            </Col>
+            <Col lg='6' md='6' className='text-end'>
+              <img src={counterImg} alt=''/>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='new__arrivals'>
+        <Container>
+          <Row>
+            <Col lg="12" className='text-center mb-5'>
+              <h2 className='section__title'>New Arrivals</h2>
+            </Col>
+            <ProductsList data={mobileProducts}/>
+            <ProductsList data={wirelessProducts}/>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='popular__category'>
+        <Container>
+          <Row>
+            <Col lg="12" className='text-center mb-5'>
+              <h2 className='section__title'>Popular in Category</h2>
+            </Col>
+            <ProductsList data={popularProducts}/>
           </Row>
         </Container>
       </section>
